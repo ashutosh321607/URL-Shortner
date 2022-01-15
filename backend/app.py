2546361ae23b2e297ed8912806c5b92957513342
milindkesar <milindkesar@gmail.com>
2022-01-15 23:26:03 +0530

    backend in flask

---
+++ backend/app.py
@@ -0,0 +1,85 @@
+from flask import Flask, render_template, request, flash
+from flask_sqlalchemy import SQLAlchemy
+import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://user_milind:password@localhost/db_testlearn'
+db = SQLAlchemy(app)
+
+
+def convert_to_python_datetime(date_in):
+  date_processing = date_in.replace('T', '-').replace(':', '-').split('-')
+  date_processing = [int(v) for v in date_processing]
+  date_out = datetime.datetime(*date_processing)
+  return date_out
+
+class URLTable(db.Model):
+  __table_name__ ='urltable'
+  # id = db.Column(db.Integer, primary_key=True)
+  user_id = db.Column(db.String(80), unique=False, nullable=False)
+  original_url = db.Column(db.String(800),nullable=False)
+  shorten_url = db.Column(db.String(400), nullable=False, primary_key=True)
+  expire_time = db.Column(db.DateTime, nullable=False)
+  created_time = db.Column(db.DateTime, nullable=False)
+  personalized = db.Column(db.Boolean)
+
+  def __init__(self, user_id, original_url, shorten_url, expire_time, created_time, personalized):
+    self.user_id = user_id
+    self.original_url = original_url
+    self.shorten_url = shorten_url
+    self.expire_time = expire_time
+    self.created_time = created_time
+    self.personalized = personalized
+
+
+@app.route('/', methods=['POST','GET'])
+def home():
+    return render_template("index.html")
+
+
+@app.route("/profile", methods=['POST','GET'])
+def profile():
+    if request.method == 'POST':
+      username = request.form["username"]
+      password = request.form["password"]
+      user_data = URLTable.query.filter_by(user_id = username).all()
+    else:
+      user_data=[]
+
+
+    return render_template("profile.html",user_data=user_data, username = username, password = password)
+    # entry = URL_Table(pname, color)
+    # db.session.add(entry)
+    # db.session.commit()
+@app.route("/confirm", methods=['POST','GET'])
+def confirm():
+  if request.method == 'POST':
+    username = request.form["username"]
+    password = request.form["password"]
+    original_url = request.form["original_url"]
+    try:
+      expiry_time = convert_to_python_datetime(request.form["expiry_time"])
+    except:
+      expiry_time = datetime.datetime.now() + datetime.timedelta(days=30)
+
+    if request.form["personalized"] is None:
+      personalized = False
+    else:
+      temp = request.form["personalized"]
+      if temp == 'False':
+        personalized = False
+      else:
+        personalized = True
+    shorten_url = "temporary123456789"
+    created_time = datetime.datetime.now()
+    entry = URLTable(user_id=username, original_url=original_url, expire_time=expiry_time, created_time=created_time,
+                      shorten_url=shorten_url, personalized=personalized)
+    db.session.add(entry)
+    db.session.commit()
+    # logic for database entry
+    return render_template("confirm.html", username = username, password=password)
+
+
+if __name__ == '__main__':
+  db.create_all()
+  app.run(debug=True)
